aebdc264d8078a361342fa2243ced9624d7d1d9b
jomoabhi <55912822+jomoabhi@users.noreply.github.com>
2021-08-19 12:21:52 +0530

    initial-commit2

--- src/Components/layout/Header.js
+++ src/Components/layout/Header.js
@@ -6,7 +6,10 @@ const Header = (props) => {
   return (
     <nav className="navbar navbar-expand-sm navbar-dark bg-danger mb-3 py-0">
       <div className="container">
-        <a href="/" className="navbar-brand">
+        <a
+          href="https://jomoabhi.github.io/ContactManager/"
+          className="navbar-brand"
+        >
           {branding}{' '}
         </a>
         <div>
